143ef8b137e67d0b4bc16e87d1f1df5c131e9d61
HankHill <hhc3h8@gmail.com>
2023-01-21 17:12:25 -0600

    working with docs on basic stuff will flesh out project tomorrow

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
@@ -6,8 +8,17 @@ class Post(models.Model):
   post_date = models.DateField('date published')
   votes = models.IntegerField(default=0)
 
+  def __str__(self):
+    return self.post_text
+  
+  def was_published_recently(self):
+    return self.post_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Comment (models.Model):
   post = models.ForeignKey(Post,
   on_delete=models.CASCADE)
   answer_text = models.CharField(max_length=3000)
   votes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.answer_text
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-  return HttpResponse("Hello World")
+  recent_post_list = Post.objects.order_by('-post_date')[:5]
+  output = ','.join([p.post_text for p in recent_post_list])
+  return HttpResponse(output)
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
+
+def results(request, post_id):
+    response = "You're looking at the results of post %s."
+    return HttpResponse(response % post_id)
+
+def vote(request, post_id):
+    return HttpResponse("You're voting on post %s." % post_id)
